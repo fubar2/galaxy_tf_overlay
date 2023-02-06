a82ea15fee7b34938efb76afafdd568039825794
fubar2 <ross.lazarus@gmail.com>
2023-02-07 09:13:44 +1100

    remove typo

--- scripts/firstrun.sh
+++ scripts/firstrun.sh
@@ -10,18 +10,9 @@ export GALAXY_VIRTUAL_ENV=$HERE/venv
 GALAXY_VIRTUAL_ENV=$HERE/venv
 echo $GALAXY_VIRTUAL_ENV
 python3 -m venv $GALAXY_VIRTUAL_ENV
-git clone https://github.com/galaxyproject/galaxy.git
-mv galaxy/* $HERE
-rm -rf galaxy
-git checkout release_23.0
-# know to work: 27cd9bb68b45175fc724e4d233aedf43bbd5f059
-git clone --depth 1 https://github.com/fubar2/galaxy_tf_overlay.git
-cp -rvu galaxy_tf_overlay/* ./
-
 sh scripts/common_startup.sh --no-create-venv
 . venv/bin/activate
 pip3 install bioblend ephemeris planemo
-git clone
 python3 scripts/tfsetup.py --galaxy_root $HERE
 echo "Your dev server is ready to run. \
 Use GALAXY_VIRTUAL_ENV=$HERE/venv && sh run.sh --skip-client-build --daemon for example. \
