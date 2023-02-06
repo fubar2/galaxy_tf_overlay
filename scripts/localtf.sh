6456ec62bea2075b2cada93b1f23131b44fb8b44
fubar2 <ross.lazarus@gmail.com>
2023-02-07 10:49:21 +1100

    fix localtf.sh

--- scripts/localtf.sh
+++ scripts/localtf.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+!/usr/bin/bash
 echo "First run takes a while. Go for a walk or do something else more useful than watching"
 git clone https://github.com/galaxyproject/galaxy.git galaxytf
 cd galaxytf
@@ -14,18 +14,22 @@ export GRAVITY_STATE_DIR=$HERE/database
 export NODE_PATH=$HERE/venv/lib/node_modules
 export NODE_VIRTUAL_ENV=$HERE/venv
 export NPM_CONFIG_PREFIX=$HERE/venv
-export GALAXY_VIRTUAL_ENV=$HERE/venv
+GALAXY_VIRTUAL_ENV=$HERE/venv
 export GALAXY_VIRTUAL_ENV=$GALAXY_VIRTUAL_ENV
 echo $GALAXY_VIRTUAL_ENV
 python3 -m venv $GALAXY_VIRTUAL_ENV
-sh scripts/common_startup.sh --no-create-venv
+GALAXY_VIRTUAL_ENV=$GALAXY_VIRTUAL_ENV && sh scripts/common_startup.sh --no-create-venv
 . venv/bin/activate
 pip3 install bioblend ephemeris planemo
-python3 scripts/tfsetup.py --galaxy_root $HERE
-# cd /tmp && planemo test --biocontainers --galaxy_root=$HERE $HERE/local_tools/tacrev/tacrev.xml
+GALAXY_VIRTUAL_ENV=$GALAXY_VIRTUAL_ENV && python3 scripts/tfsetup.py --galaxy_root $HERE
 echo "Your dev server is ready to run. \
 Use GALAXY_VIRTUAL_ENV=$HERE/venv && sh run.sh --skip-client-build --daemon for example. \
 Local web browser url is http://localhost:8080. Admin already exists.\
 Admin login is toolfactory@galaxy.org with ChangeMe! as the temporary password. \
 Please do change it. Do not expose this development server on the open internet please.
 It has none of the layers of isolation that a secure public server needs."
+
+
+
+
+
