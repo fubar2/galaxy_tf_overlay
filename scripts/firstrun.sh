adc5ee3996367db09f5416685457ef83aa1658dd
fubar2 <ross.lazarus@gmail.com>
2023-03-06 14:56:30 +1100

    fix firstrun - use galaxy_root fool.

--- scripts/firstrun.sh
+++ scripts/firstrun.sh
@@ -1,18 +1,14 @@
+#!/usr/bin/env bash
 # ansible command calls this to do most of the work
 # no need to make this into a playbook
-HERE=`pwd`
-echo "$HERE"
-GALAXY_VIRTUAL_ENV=$HERE/venv
 echo $GALAXY_VIRTUAL_ENV
-# fresh db so need clean jobs?
-rm -rf database/jobs_directory/*
-python3 -m venv $GALAXY_VIRTUAL_ENV
-sh scripts/common_startup.sh --no-create-venv
+service postgresql start
 . $GALAXY_VIRTUAL_ENV/bin/activate
+cd $GALAXY_ROOT
 pip3 install -U bioblend ephemeris planemo
-python3 scripts/tfsetup.py --galaxy_root $HERE
+python3 $GALAXY_ROOT/scripts/tfsetup.py --galaxy_root $GALAXY_ROOT --force
 echo "Your dev server is ready to run. \
-Use GALAXY_VIRTUAL_ENV=$HERE/venv && sh run.sh --skip-client-build --daemon for example. \
+Use GALAXY_VIRTUAL_ENV=$GALAXY_VIRTUAL_ENV/venv && sh run.sh --daemon for example. \
 Local web browser url is http://localhost:8080. Admin already exists.\
 Admin login is toolfactory@galaxy.org with ChangeMe! as the temporary password. \
 Please do change it. Do not expose this development server on the open internet please. \
