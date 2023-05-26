319aa6646abf20247536d7b646455f73ba5f25c0
fubar2 <ross.lazarus@gmail.com>
2023-05-26 11:19:08 +1000

    fix name of overlay zip directory again.

--- localtf_docker.sh
+++ localtf_docker.sh
@@ -2,7 +2,7 @@
 # Docker version - galaxy has already been installed - so galaxy_root is the first parameter
 # assume run from the git galaxy_tf_overlay clone directory
 GALAXY_ROOT=$1
-OVERLAY=$1/galaxy_tf_overlay
+OVERLAY=$1/galaxy_tf_overlay-main
 echo "Using galaxy_root = $GALAXY_ROOT"
 
 OVERZIP="https://github.com/fubar2/galaxy_tf_overlay/archive/refs/heads/main.zip"
@@ -11,6 +11,7 @@ USE_DB_URL="sqlite:///$OURDIR/database/universe.sqlite?isolation_level=IMMEDIATE
 cd $GALAXY_ROOT
 wget $OVERZIP
 unzip "main.zip"
+mkdir -p $GALAXY_ROOT/config/plugins/notinusenowvisualizations/
 mv $GALAXY_ROOT/config/plugins/visualizations/* $GALAXY_ROOT/config/plugins/notinusenowvisualizations/
 # save building them while testing
 
@@ -25,7 +26,7 @@ GALAXY_VIRTUAL_ENV=$GALAXY_ROOT/.venv
 export GALAXY_VIRTUAL_ENV=$GALAXY_ROOT/.venv
 . $GALAXY_VIRTUAL_ENV/bin/activate
 pip3 install -U bioblend ephemeris planemo watchdog
-python3 scripts/tfsetup.py --galaxy_root $OURDIR --galaxy_venv $GALAXY_VIRTUAL_ENV --db_url $USE_DB_URL --force
+python3 $GALAXY_ROOT/scripts/tfsetup.py --galaxy_root $GALAXY_ROOT --galaxy_venv $GALAXY_VIRTUAL_ENV --db_url $USE_DB_URL --force
 find $OURDIR -name '*.pyc' -delete | true \
 find /usr/lib/ -name '*.pyc' -delete | true \
 find $GALAXY_VIRTUAL_ENV -name '*.pyc' -delete | true \
