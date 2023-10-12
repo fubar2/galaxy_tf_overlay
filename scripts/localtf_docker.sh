1ef6bdbb689732b30457160ae2e08a6dda9683a8
fubar2 <ross.lazarus@gmail.com>
2023-10-12 16:05:20 +1100

    test new style localtf.sh in docker

--- scripts/localtf_docker.sh
+++ scripts/localtf_docker.sh
@@ -15,7 +15,7 @@ USE_DB_URL="sqlite:///$1/database/universe.sqlite?isolation_level=IMMEDIATE"
 cd $GALAXY_ROOT
 mkdir -p $GALAXY_ROOT/database_copy $GALAXY_ROOT/local_tools_copy
 cp -rv $OVERLAY/config/* $GALAXY_ROOT/config/
-cp -rv $OVERLAY/configdocker/* $GALAXY_ROOT/config # replace normal one
+# cp -rv $OVERLAY/configdocker/* $GALAXY_ROOT/config # replace normal one
 cp -rv $OVERLAY/local $GALAXY_ROOT/
 cp -rv $OVERLAY/local_tools $GALAXY_ROOT/
 cp -rv $OVERLAY/static/* $GALAXY_ROOT/static/
@@ -35,25 +35,42 @@ sed -i "s~^  data_dir:.*~  data_dir: $OURDIR/database~g" $OURDIR/config/galaxy.y
 
 
 export GALAXY_VIRTUAL_ENV=$1/.venv
+cd $OURDIR
+export GALAXY_VIRTUAL_ENV=$GALAXY_VIRTUAL_ENV
 export GALAXY_INSTALL_PREBUILT_CLIENT=1
-. /tmp/venv2/bin/activate
-pip install bioblend ephemeris
-python3 $OVERLAY/scripts/tfsetup.py --galaxy_root $GALAXY_ROOT --galaxy_venv $GALAXY_VIRTUAL_ENV --db_url $USE_DB_URL --force
-
-VENV2=$OURDIR/.venv2
+GALAXY_INSTALL_PREBUILT_CLIENT=1
 python3 -m venv $GALAXY_VIRTUAL_ENV
-# needed for 23.1 because of packaging legacy_ changes...
-GALAXY_INSTALL_PREBUILT_CLIENT=1 && bash scripts/common_startup.sh --no-create-venv
-export PYTHONPATH=
+GALAXY_INSTALL_PREBUILT_CLIENT=1 && bash $OURDIR/scripts/common_startup.sh --no-create-venv
 rm -rf $VENV2
 python3 -m venv $VENV2
-. /tmp/venv2/bin/activate
-pip install -U bioblend ephemeris planemo galaxyxml
+. $VENV2/bin/activate && pip install bioblend ephemeris
+
+bash run.sh --daemon && sleep 30
+. $VENV2/bin/activate && export PYTHONPATH=$GALAXY_VIRTUAL_ENV/lib/python3.10/site-packages/ \
+  && python3 scripts/tfsetup.py --galaxy_root $OURDIR --galaxy_venv $GALAXY_VIRTUAL_ENV --db_url $USE_DB_URL --force
 deactivate
-cd $OURDIR
-bash run.sh --daemon && sleep 40
-echo "PYTHONPATH=PYTHONPATH:$VENV2/lib/python3.10/site-packages:$GALAXY_VIRTUAL_ENV/lib/python3.10/site-packages"
-export PYTHONPATH=PYTHONPATH:$VENV2/lib/python3.10/site-packages && \
-  python3 scripts/tfsetup.py --galaxy_root $OURDIR --galaxy_venv $GALAXY_VIRTUAL_ENV --db_url $USE_DB_URL --force
-export PYTHONPATH=
 bash run.sh --stop-daemon
+
+
+#export GALAXY_INSTALL_PREBUILT_CLIENT=1
+#. /tmp/venv2/bin/activate
+#pip install bioblend ephemeris
+#python3 $OVERLAY/scripts/tfsetup.py --galaxy_root $GALAXY_ROOT --galaxy_venv $GALAXY_VIRTUAL_ENV --db_url $USE_DB_URL --force
+
+#VENV2=$OURDIR/.venv2
+#python3 -m venv $GALAXY_VIRTUAL_ENV
+## needed for 23.1 because of packaging legacy_ changes...
+#GALAXY_INSTALL_PREBUILT_CLIENT=1 && bash scripts/common_startup.sh --no-create-venv
+#export PYTHONPATH=
+#rm -rf $VENV2
+#python3 -m venv $VENV2
+#. /tmp/venv2/bin/activate
+#pip install -U bioblend ephemeris planemo galaxyxml
+#deactivate
+#cd $OURDIR
+#bash run.sh --daemon && sleep 40
+#echo "PYTHONPATH=PYTHONPATH:$VENV2/lib/python3.10/site-packages:$GALAXY_VIRTUAL_ENV/lib/python3.10/site-packages"
+#export PYTHONPATH=PYTHONPATH:$VENV2/lib/python3.10/site-packages && \
+  #python3 scripts/tfsetup.py --galaxy_root $OURDIR --galaxy_venv $GALAXY_VIRTUAL_ENV --db_url $USE_DB_URL --force
+#export PYTHONPATH=
+#bash run.sh --stop-daemon
