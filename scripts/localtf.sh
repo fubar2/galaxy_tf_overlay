6763b17c21c877ad9de94cf95bdc8694bc51ef15
fubar2 <ross.lazarus@gmail.com>
2024-06-04 20:44:45 +1000

    venv?!

--- scripts/localtf.sh
+++ scripts/localtf.sh
@@ -52,7 +52,7 @@ sed -i "s~^  data_dir:.*~  data_dir: $GALAXY_ROOT/database~g" $GALAXY_ROOT/confi
 export GALAXY_INSTALL_PREBUILT_CLIENT=1
 
 GALAXY_INSTALL_PREBUILT_CLIENT=1 && bash $GALAXY_ROOT/scripts/common_startup.sh --no-create-venv \
-  && $GALAXY_VIRTUAL_ENV/bin/galaxyctl start && sleep 20 \
+  && $GALAXY_VIRTUAL_ENV/bin/galaxyctl start && sleep 20 && . $GALAXY_VIRTUAL_ENV/bin/activate \
   && python3 $GALAXY_ROOT/scripts/tfsetup.py --galaxy_root $GALAXY_ROOT --galaxy_venv $GALAXY_VIRTUAL_ENV --db_url $USE_DB_URL --force 
 $GALAXY_VIRTUAL_ENV/bin/galaxyctl stop
 deactivate
